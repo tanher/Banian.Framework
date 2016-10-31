5331354f51bee43945784a044faed25f60af2660
tanher <tanher@qq.com>
2016-11-01 00:18:22 +0800

    添加项目文件。

---
+++ Banian.Infrastructure.Web/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Banian.Infrastructure.Web")]
+[assembly: AssemblyTrademark("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("183d99a7-c672-4241-bb28-b0217308c424")]
---
+++ src/Sample/Sample.Web/Startup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Banian.Infrastructure.Web;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace Sample.Web
+{
+    public class Startup : BanianStartup
+    {
+        public Startup(IHostingEnvironment env, ILoggerFactory loggerFactory)
+            : base(env, loggerFactory)
+        {
+        }
+    }
+}
