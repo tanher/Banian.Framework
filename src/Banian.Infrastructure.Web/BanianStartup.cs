c0bc531ee52466248a63c82ada09c3c1423e4294
tanher <tanher@qq.com>
2016-11-15 00:34:01 +0800

    Update:Thomas-添加NLog配置。

--- src/Banian.Infrastructure.Web/BanianStartup.cs
+++ src/Banian.Infrastructure.Web/BanianStartup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using NLog.Extensions.Logging;
 
 namespace Banian.Infrastructure.Web
 {
@@ -31,8 +32,8 @@ namespace Banian.Infrastructure.Web
         
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
-            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
-            loggerFactory.AddDebug();
+            loggerFactory.AddNLog();
+            env.ConfigureNLog("nlog.config");
 
             if (env.IsDevelopment())
             {
